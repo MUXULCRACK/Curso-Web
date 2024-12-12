46fe631e593d9778be438a833c9d322b923f48b6
MUXULCRACK <jhimmy.cuaran@unipamplona.edu.co>
2024-12-12 13:39:33 -0500

    first commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+  /* METODO PARA VER JSON JUNTO CON CONTROLADOR
+  @Get()
+  getHello(){
+    return this.appService.getHello();
+  }*/
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,11 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! Jhimmy Antonio Cuaran Rodriguez';
   }
+  /* METODO PARA VER JSON JUNTO CON SERVICIOS
+  getHello() {
+    return {'Hello World! Jhimmy Antonio Cuaran Rodriguez'
+    };
+  }*/
 }
